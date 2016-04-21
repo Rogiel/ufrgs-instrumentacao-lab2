f52db30e12723352c592a24d0190f696b693604e
Rogiel Sulzbach <rogiel@rogiel.com>
2016-04-21 15:37:48 -0300

    Melhorada a apresentação dos resultados do pêndulo

--- Resources/MATLAB/primeiro.m
+++ Resources/MATLAB/primeiro.m
@@ -1,4 +1,4 @@
-a=importdata('../Data/Experimento1.lvm');
+a=importdata('../Data/Experimento7.lvm');
 
 t = a(:, 1);
 data = a(:, 2);
@@ -6,21 +6,48 @@ data = a(:, 2);
 close all; 
 fourierTransform=fft(data);
 
-sample_rate=100;
+sample_rate=1/(t(2) - t(1));
 w=0:sample_rate/(length(data) - 1):sample_rate;
 
-fig1=figure(1);
+fig=figure();
 plot(t,data);
-title ('Amplitude do pendulo em funcao de tempo');
+title ('Tensao eletica do pendulo em funcao de tempo');
 xlabel('t(s)');
-ylabel('Amplitude');
+ylabel('Volts');
 grid on;
 
 set(gcf, 'PaperPosition', [0 0 10 5]);
 set(gcf, 'PaperSize', [10 5]);
-saveas(fig1,'images/time-plot','pdf')
+saveas(fig,'images/time-plot-raw','pdf')
 
-fig2=figure(2);
+[b,a] = butter(10,20 / sample_rate);
+data = filter(b, a, data);
+
+fig=figure();
+plot(t,data);
+title ('Tensao eletica do pendulo em funcao de tempo');
+xlabel('t(s)');
+ylabel('Volts');
+grid on;
+
+set(gcf, 'PaperPosition', [0 0 10 5]);
+set(gcf, 'PaperSize', [10 5]);
+saveas(fig,'images/time-plot','pdf')
+
+angle = -4.3 + 34 .* data;
+
+fig=figure();
+plot(t,angle);
+title ('Inclinacao do pendulo em funcao de tempo');
+xlabel('t(s)');
+ylabel('Graus');
+grid on;
+
+set(gcf, 'PaperPosition', [0 0 10 5]);
+set(gcf, 'PaperSize', [10 5]);
+saveas(fig,'images/angle-time-plot','pdf')
+
+fig=figure();
 plot(w, abs(fourierTransform));
 title ('Analise em dominio de frequencia');
 xlabel('f(Hz)');
@@ -29,8 +56,19 @@ grid on;
 
 set(gcf, 'PaperPosition', [0 0 10 5]);
 set(gcf, 'PaperSize', [10 5]);
-saveas(fig2,'images/frequency-plot','pdf')
+saveas(fig,'images/frequency-plot','pdf')
 
+
+fig=figure();
+plot(w(1:12), abs(fourierTransform(1:12)));
+title ('Analise em dominio de frequencia');
+xlabel('f(Hz)');
+ylabel('Amplitude');
+grid on;
+
+set(gcf, 'PaperPosition', [0 0 10 5]);
+set(gcf, 'PaperSize', [10 5]);
+saveas(fig,'images/frequency-plot-slice','pdf')
 
 
 %N = 2^nextpow2(length(y1));
